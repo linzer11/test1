938034641f487bd7ae4096a4ac8e081c2abf1a4d
linzer11 <jeonghu949@gmail.com>
2025-06-05 15:48:04 +0900

    Update 00_lin.py

--- pages/00_lin.py
+++ pages/00_lin.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.image(https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHV0ajlhcWg4eG4xeGhjdTlmM3M0bXBtZWRnZnY4aTl5anFsMDhsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26gYKaUMw1IM2JeeI/giphy.gif)
+st.image("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHV0ajlhcWg4eG4xeGhjdTlmM3M0bXBtZWRnZnY4aTl5anFsMDhsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26gYKaUMw1IM2JeeI/giphy.gif")
